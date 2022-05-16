a45606eeb1bf7bbb5e80c81a932222820a95f8cd
kyok01 <kazutakayokoils@gmail.com>
2022-05-16 09:41:27 +0900

    .env-exmaple.js added

---
+++ js/env-example.js
@@ -0,0 +1,12 @@
+// leave your apiKey in
+export let envApiKey = "AIzaSyBnz1A7GtcrJ_3ZA8EiMJlcCFZItQs3OC8";
+// leave your authDomain in
+export let envAuthDomain = "gamedemo-2022-05-15.firebaseapp.com";
+// leave your projectId in
+export let envProjectId = "gamedemo-2022-05-15";
+// leave your storageBucket in
+export let envStorageBucket = "gamedemo-2022-05-15.appspot.com";
+// leave your messagingSenderId in
+export let envMessagingSenderId = "171659471230";
+// leave your appId in
+export let envApiId = "1:171659471230:web:6d3b541ec0cbaee2ef6b2f";
